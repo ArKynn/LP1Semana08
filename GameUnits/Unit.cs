277eeaea990cb713ec715f52d383150b049a187f
David Mendes <david.f.mendes@proton.me>
2023-05-03 16:01:29 +0100

    Added and modified Unit class

---
+++ GameUnits/Unit.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.Design;
+
+namespace GameUnits;
+
+public abstract class Unit
+{
+    private int movement;
+    public virtual int Health { get; set; }
+    public abstract float  Cost { get; }
+
+    public Unit(int movement)
+    {
+        this.movement = movement;
+    }
+
+    public void Move()
+    {
+        Console.WriteLine($"This unit moved {movement} times.");
+    }
+}
