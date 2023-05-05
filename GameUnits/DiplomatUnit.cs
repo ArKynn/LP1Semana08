e927e45d43642765f95ad19818549b0259bcc99c
David Mendes <david.f.mendes@proton.me>
2023-05-05 14:07:35 +0100

    Added completed DiplomatUnit class

---
+++ GameUnits/DiplomatUnit.cs
@@ -0,0 +1,29 @@
+﻿namespace GameUnits;
+
+public class DiplomatUnit : XPUnit
+{
+    protected sealed override float Cost { get; init; }
+
+    public DiplomatUnit(int movement, int health) : base(movement, health)
+    {
+        Cost = 7.1f;
+        Movement = 6;
+        Health = 1;
+    }
+
+    public void MakeAgreement(Unit unit)
+    {
+        if (unit is DiplomatUnit)
+        {
+            XP += 3;
+        }
+        else if (unit is SettlerUnit)
+        {
+            XP += 2;
+        }
+        else
+        {
+            XP++;
+        }
+    }
+}
